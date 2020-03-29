ff97f9796f410a5ccd7357c49b2b9f7dfd7ea342
Sergey Skumatov <sergeyskumatov@aol.com>
2020-03-29 14:49:25 -0600

    Javascript

---
+++ home.js
@@ -0,0 +1,14 @@
+console.log('hello');
+
+// alert("this is Sergey");
+
+// Variables
+var b = "smoothie";
+console.log(b);
+
+var someNumber = 45;
+console.log(someNumber);
+
+var age = prompt("what is your age?");
+
+document.getElementById('someText').innerHTML = age;
